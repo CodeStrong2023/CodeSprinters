490f3046c2101a2379cd3221f33fffe3910d9d22
Enzo Genaro Ferrero <genaroenzo4@gmail.com>
2023-09-12 10:31:26 -0300

    Clase N°4 JavaScript

--- Genaro-Ferrero/JavaScript/leccion02/02-01-TiposDatos.js
+++ Genaro-Ferrero/JavaScript/leccion02/02-01-TiposDatos.js
@@ -5,16 +5,41 @@ La sintaxis en los comentarios es similar a  la sintaxis de java
 
 // Tipo String -"Str"
 var nombre = "Gena";
-console.log(nombre);
+console.log(typeof nombre);
+nombre = 7; // Las variables en JavaScripts son dinamicas
+console.log(typeof nombre);
+nombre = 12.3; 
+console.log(typeof nombre); // Una variable tipo float es igual a una variable tipo numerica en JavaScripts
 
 // Tipo Numérico
 var numero = 3000;
 console.log(numero);
 
-// Tipo objeto "objetc"
+// Tipo objeto 
 var objeto = {
     nombre: "Enzo",
     apellido: "Ferrero",
     telefono: "2604679904"
 };
 console.log(objeto);
+
+// Tipo Booleano (Bandera)
+var Bandera = true;
+console.log(Bandera);
+
+// Tipo de dato funcion
+function mifuncion(){}
+console.log(typeof mifuncion);
+
+// Tipo de dato symbol
+var simbolo = Symbol("Mi simbolo");
+console.log(typeof simbolo);
+
+// Tipo de dato clase (Es una funcion)
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona)
