3f8d342fa9c12c5aec04ee559cc5856cf5da7a8c
Enzo Genaro Ferrero <genaroenzo4@gmail.com>
2023-09-13 18:09:16 -0300

    Se corriguieron errores Clase N°4 JAVA

--- Genaro-Ferrero/Java/JavaEjercicios/Ejercicios/Ejercicio7OptionPane.java
+++ Genaro-Ferrero/Java/JavaEjercicios/Ejercicios/Ejercicio7OptionPane.java
@@ -1,23 +1,22 @@
 import javax.swing.JOptionPane;
+
 public class Ejercicio7OptionPane {
     // Pedir números hasta que se introduzca uno negativo y calcular la media 
     public static void main(String[] args) {
         System.out.println(" Ejercicio N° 7- OptionPane");
-        int numero ,conteo = 0, suma = 0;
+        int numero, conteo = 0, suma = 0;
         float promedio = 0;
         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
-        while (numero >= 0){
-            suma+= numero;
+        while (numero >= 0) {
+            suma += numero;
             conteo++;
             numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
-        if (conteo==o){
+        }
+        if (conteo == 0) {
             System.out.println("\n Error, La division entre cero no existe");
-        }
-        else{
-            promedio = (float)suma/conteo;
-            System.out.println("\n El promedio es: "+ promedio);
+        } else {
+            promedio = (float) suma / conteo;
+            System.out.println("\n El promedio es: " + promedio);
         }
     }
-    }
-
-
+}
