1f67fd210d71c839d334b31e728a1f13a09238ed
Matias Moyano <moyano.matiasnicolas.mm@gmail.com>
2023-09-29 11:50:03 -0300

    clase 2 y 3 javascript

---
+++ Matias-Moyano/Javascript/Clase3/tiposDeDatos.js
@@ -0,0 +1,26 @@
+/* Comentarios */
+// Comentarios de una linea
+/* Comentarios de varias lineas
+    por ejemplo este */
+
+var nombre = "Matias"; // String
+console.log(nombre)
+
+var edad = 25; // Number
+console.log(edad)
+
+var sueldo = 25.5; // Number
+console.log(sueldo)
+
+var objeto = {
+    nombre: "Matias",
+    edad: 25,
+    sueldo: 25.5,
+    casado: false,
+    hijos: null,
+    mascota: {
+        nombre: "Shaggy",
+    }
+}; // Object
+
+console.log(objeto)
