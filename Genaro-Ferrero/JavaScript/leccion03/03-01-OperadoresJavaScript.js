82fe49985702fabebdb8a281020c3820a26d940b
Enzo Genaro Ferrero <genaroenzo4@gmail.com>
2023-10-25 17:32:21 -0300

    Clase N°9 JavaScript

--- Genaro-Ferrero/JavaScript/leccion03/03-01-OperadoresJavaScript.js
+++ Genaro-Ferrero/JavaScript/leccion03/03-01-OperadoresJavaScript.js
@@ -25,3 +25,42 @@ if (dentroRango >= valMin && dentroRango <= valMax){
 else{
     console.log("Esta fuera del rango ")
 }
+
+// Ejercicio Si el padre puede asistir al juego de su hijo
+let vacaciones = false , diaDescanso =false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+}
+else{
+    console.log("Es padre no puede asistir al juego de su hijo")
+}
+
+// Ejercicio Operador ternario 
+let resultado2 = 3 > 2 ? "Verdadero": "Falso";
+console.log(resultado2)
+
+let numero = 9;
+resultado2 = numero % 2 == 0 ? "Es un número Par" : "Es un numero Impar ";
+console.log(resultado2)
+
+// Convertir String a Numero
+let miNumero = "10";
+console.log(miNumero)
+let edad2= Number(miNumero);// Funcion de convertir de String a Numero
+
+// Funcion isNan
+if (isNaN(edad2)){
+    console.log("Esta variable no contiene Números")
+}
+else{
+    if(edad2 >= 18){
+        console.log("Puede votar");
+}
+    else{
+        console.log("No puede votar");
+}
+}
+
+let resultado3 = edad2 >=18 ? "Puede votar" : "No puede votar";
+console.log(resultado3)
+
