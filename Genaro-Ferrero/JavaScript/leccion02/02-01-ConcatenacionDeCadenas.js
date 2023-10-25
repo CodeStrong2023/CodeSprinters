aaf8fb04be381df5332e2b45ea39a33b1ac0d3b6
Enzo Genaro Ferrero <genaroenzo4@gmail.com>
2023-10-25 15:23:54 -0300

    Clase N° 6 JavaScript

---
+++ Genaro-Ferrero/JavaScript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,16 @@
+var nombre = "Jose";
+var apellido = "Montes";
+var nombreCompleto = nombre+ " "+apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = "Genaro"+ " "+ "Ferrero"
+console.log(nombreCompleto2)
+var juntos = nombre + 219; // Lee de izq a derecha 
+console.log(juntos);
+juntos = nombre + 78 + 17;
+console.log(juntos);
+juntos = 78 + 17 + nombre;
+console.log.log(juntos);
+
+nombre += apellido; 
+console.log(nombre)
+
