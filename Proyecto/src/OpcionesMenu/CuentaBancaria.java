ef88c85494f1d2019b61908fa89bb0a6b37f9922
Enzo Genaro Ferrero <genaroenzo4@gmail.com>
2023-10-31 19:38:44 -0300

    Proyecto

---
+++ Proyecto/src/OpcionesMenu/CuentaBancaria.java
@@ -0,0 +1,118 @@
+
+package OpcionesMenu;
+
+public class CuentaBancaria {
+
+    public static void setcbu(String CBU) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    private String nombreUsuario;
+    private String contra;
+    private String nombreReal;
+    private String DNI;
+    private String sexo;
+    private int edad;
+    private double dinero;
+    private int cbu;
+
+    // Constructor completo
+
+    public CuentaBancaria( String nombreUsuario, String contra, String nombreReal, String DNI, String sexo, int edad, double dinero, int cbu) {
+        this.nombreUsuario = nombreUsuario;
+        this.contra = contra;
+        this.nombreReal = nombreReal;
+        this.DNI = DNI;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.dinero = dinero;
+        this.cbu = cbu;
+    }
+    
+    // Getters 
+    public String getNombreUsuario() {
+        return this.nombreUsuario;
+    }
+
+    public String getContra() {
+        return this.contra;
+    }
+
+    public String getNombreReal() {
+        return this.nombreReal;
+    }
+
+    public String getDNI() {
+        return this.DNI;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public double getDinero() {
+        return this.dinero;
+    }
+
+    public int getCbu() {
+        return this.cbu;
+    }
+    
+    // Setters
+    
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public void setNombreReal(String nombreReal) {
+        this.nombreReal = nombreReal;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setDinero(double dinero) {
+        this.dinero = dinero;
+    }
+
+    public void setCbu(int cbu) {
+        this.cbu = cbu;
+    }
+     // To String
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|                       Cuenta Creada con Exito                        |");
+        sb.append("| Nombre de Usuario: ").append(nombreUsuario);
+        sb.append("| Contraseña: ").append(contra);
+        sb.append("| Nombre: ").append(nombreReal);
+        sb.append("| D.N.I: ").append(DNI);
+        sb.append("| Sexo: ").append(sexo);
+        sb.append("| Edad: ").append(edad);
+        sb.append("| Saldo: ").append(dinero);
+        sb.append("| C.B.U: ").append(cbu);
+        return sb.toString();
+    }
+    
+    
+    
+   
+    }
