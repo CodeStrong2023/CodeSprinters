f9fa402e149a4542cb9e0bebe0ff544eed2f9d23
Francisco Corvalan <corvalanfrancisco86@gmail.com>
2023-11-02 14:40:42 -0300

    Clase 7 de Javascript

---
+++ francisco-corvalan/javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+// Ejercicio para encontrar numeros pares
+let parImpar = 7;
+if(parImpar % 2 == 0){
+    console.log("Es un numero par");
+}
+else{
+    console.log("Es un numero impar");
+}
+
+// Ejercicio es mayor de edad
+let edad = 16, adulto =18;
+if( edad >= adulto ){
+    console.log("Es una persona mayor de edad");
+}
+else{
+    console.log("Es una persona menor de edad");
+}
