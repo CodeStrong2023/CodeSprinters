4f46344ad0879ed45ff1226cbf791ce68d4cefe5
Francisco Corvalan <corvalanfrancisco86@gmail.com>
2023-11-02 16:03:54 -0300

    Clase 9 JavaScript

--- francisco-corvalan/javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ francisco-corvalan/javascript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -25,3 +25,40 @@ if( dentroRango >= valMin && dentroRango <= valMax ){
 else{
     console.log('Esta fuera del rango establecido');
 }
+// Ejercicio : Si el padre puede asistir al juego de su hijo
+let vacaciones = false, diaDescanso = false;
+if( vacaciones || diaDescanso ){
+    console.log("El padre puede asister al juego de su hijo");
+}
+else{
+    console.log("El padre no puede asistir al juego de su hijo");
+}
+
+// Operador Ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2);
+let numero = 12;
+resultado2 = numero % 2 == 0 ? "Es un numero PAR" : "Es un numero IMPAR";
+console.log(resultado2);
+
+//Convertir String a Number
+let miNumero = "21";
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);
+console.log(typeof edad2);
+// Funcion isNaN
+if(isNaN(edad2 >= 18)){
+    console.log("Esta variable no contiene solo numeros");
+}
+else{
+    if(edad2 >= 18){
+        console.log("Puede votar")
+    }
+    else{
+        console.log("No puede votar");
+    }
+    
+}
+
+let resultado3 = edad2 >= 18 ? "Puede votar" : "No puede votar";
+console.log(resultado3);
