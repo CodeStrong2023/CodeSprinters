17e21db0fb8c435678ba931270ebba1ce3c036a4
Matias Moyano <moyano.matiasnicolas.mm@gmail.com>
2023-11-08 11:09:48 -0300

    Clase 6 javascript

---
+++ Matias-Moyano/Javascript/Clase6/concatenacion.js
@@ -0,0 +1,18 @@
+var nombre = "Matias";
+var apellido = "Perez";
+var nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = "Matias" + " " + "Perez";
+console.log(nombreCompleto2);
+
+var x = nombre + 2 + 4;
+console.log(x);
+
+x = nombre + (2 + 4);
+console.log(x);
+
+x = 2 + 4 + nombre;
+console.log(x);
+
+nombre += " Perez";
+console.log(nombre);
